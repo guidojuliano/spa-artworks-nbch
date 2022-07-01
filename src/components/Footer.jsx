0fe19c9fb635500baf44bf9857b1fe9f5f7ae253
guidojuliano <guidojulianou@gmail.com>
2022-07-01 00:21:57 -0300

    detalles responsivos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 function Footer() {
   return (
     <Navbar bg="dark" variant="dark" className="custom-footer">
-      <p>Guido Juliano Mazzuchini Ubaldi</p>
+      <p>Guido Juliano Ubaldi</p>
       <p>SPA React - Instituto de Arte de Chicago</p>
     </Navbar>
   );
